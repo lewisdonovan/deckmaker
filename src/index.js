a408411a2e00b26829216752c0775510ada2eb21
Lewis Donovan <lewis.donovan@sonymusic.com>
2018-08-29 16:28:00 +0100

    MVP via command line

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 'use strict';
-const util = require('util');
+const exec = require('node-exec-promise').exec;
 
+//Get args from terminal command
 const args = process.argv;
 const url = args[2];
 const filename = args[3];
 
-const exec = util.promisify(require('child_process').exec);
-async function createPdf() {
-  const { stdout, stderr } = await exec('decktape '+url+' webslides '+filename);
-  console.log('stdout:', stdout);
-  console.log('stderr:', stderr);
-}
+//Sanity check
+console.log(url);
+console.log(filename);
 
-createPdf();
+//Execute the dectape command
+exec('decktape webslides '+url+' '+filename).then(function(out) {
+  console.log(out.stdout, out.stderr);
+}, function(err) {
+  console.error(err);
+});
